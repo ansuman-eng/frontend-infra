e601692a7faa54e86de3576e02cfe0681a0350a7
ansuman-eng <ansuman1998@gmail.com>
2023-11-03 14:53:13 +0530

    feat: nested html

--- barebone-react/App.js
+++ barebone-react/App.js
@@ -1,7 +1,19 @@
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement(
+    'div',
+    { id: 'child' },
+    React.createElement('h1', { id: 'heading' }, 'Nested Heading')
+  )
+);
+
 const heading = React.createElement(
   'h1',
   { id: 'heading' },
   'Hello World from React!'
 );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+// root.render(heading);
+root.render(parent);
